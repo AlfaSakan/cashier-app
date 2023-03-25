c239a6491fd8a06ce2bcc64981c8745b4b28e016
alfa <ahmad.sakan@nri.co.id>
2023-03-25 14:14:16 +0700

    fix: depedencies and package name

--- src/lib/client/components/organisms/BottomNavigation/BottomNavigation.organism.svelte
+++ src/lib/client/components/organisms/BottomNavigation/BottomNavigation.organism.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import BagShopping from '../../Icons/BagShopping.svelte';
 	import Chart from '../../Icons/Chart.svelte';
-
 	import Home from '../../Icons/Home.svelte';
 	import Person from '../../Icons/Person.svelte';
+
+	export let currentPage = '/';
 </script>
 
 <div class="btm-nav">
-	<a type="button" href="/">
+	<a type="button" class={currentPage === '/' ? 'active' : ''} href="/">
 		<Home />
 	</a>
-	<a type="button" class="active" href="/warehouse">
+	<a type="button" class={currentPage === '/warehouse' ? 'active' : ''} href="/warehouse">
 		<BagShopping />
 	</a>
 	<button>
