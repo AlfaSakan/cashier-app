548e931783d37ad3db1934c1f665b832c159fc33
alfa sakan <alfasakan11@gmail.com>
2023-03-30 15:40:26 +0700

    fix: cookie domain

--- src/lib/server/constants/env.constant.ts
+++ src/lib/server/constants/env.constant.ts
@@ -1,4 +1,5 @@
 export const env = {
 	jwtKey: import.meta.env.VITE_JWT_KEY || '',
-	salt: import.meta.env.VITE_SALT || ''
+	salt: import.meta.env.VITE_SALT || '',
+	domain: import.meta.env.VITE_DOMAIN || 'localhost'
 } as const;
--- src/routes/(app)/register/+page.server.ts
+++ src/routes/(app)/register/+page.server.ts
@@ -1,5 +1,6 @@
 import { cookiesKey } from '$lib/client/constants/cookies.constant';
 import type { SignUpDto } from '$lib/schema/session.schema';
+import { env } from '$lib/server/constants/env.constant';
 import { authService, userService } from '$lib/server/service';
 import { error, fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
@@ -27,8 +28,8 @@ export const actions: Actions = {
 
 		if (loginData.error) throw error(500, { message: loginData.error });
 
-		cookies.set(cookiesKey.accessKey, loginData.token.accessToken, { path: '/', httpOnly: true });
-		cookies.set(cookiesKey.refreshKey, loginData.token.refreshToken, { path: '/', httpOnly: true });
+		cookies.set(cookiesKey.accessKey, loginData.token.accessToken, { domain: env.domain });
+		cookies.set(cookiesKey.refreshKey, loginData.token.refreshToken, { domain: env.domain });
 
 		throw redirect(303, '/');
 	}
