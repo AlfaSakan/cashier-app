c8a2f348c81a465aff280a0a4a230a6b84a767f1
alfa sakan <alfasakan11@gmail.com>
2023-04-08 01:38:37 +0700

    fix: styling layout app and bottom transaction

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-	import { HeaderNavigation } from '$lib/client/components';
+	import { HeaderNavigation, SidebarNavigation } from '$lib/client/components';
+	import { themeStore } from '$lib/client/store/theme.store';
 	import { Toaster } from 'svelte-french-toast';
+
+	let inputRef: HTMLInputElement;
+	$: checkedTheme = $themeStore === 'emerald';
+
+	function closeSidebar() {
+		inputRef.checked = false;
+	}
+
+	function handleThemeToggle() {
+		const dataTheme = checkedTheme ? 'dark' : 'emerald';
+		document.documentElement.setAttribute('data-theme', dataTheme);
+		localStorage.setItem('data-theme', dataTheme);
+		$themeStore = dataTheme;
+	}
 </script>
 
 <div class="drawer drawer-mobile">
@@ -13,4 +28,6 @@
 		</div>
 		<Toaster />
 	</div>
+
+	<SidebarNavigation {closeSidebar} onThemeToggle={handleThemeToggle} {checkedTheme} />
 </div>
